6b4eec5d577d1bd1bea7bc9361a720cfc519d8d6
kemi msabeni <kemimsabeni@gmail.com>
2023-02-27 00:46:46 +0300

    work on navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'razzmatazz-pink': '#E91E63',
+        'davys-gray': '#555555',
+        'blaze-orange': '#fc6a03'
+      },
+    },
   },
   plugins: [],
 }
